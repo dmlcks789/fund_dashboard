9059015d5a578d75138ba33ca27ac6c78cb85851
dmlcks789 <dmlcks789@naver.com>
2026-05-26 11:05:10 +0900

    최소 PUSH

---
+++ README.md
@@ -0,0 +1,5 @@
+## 프론트 실행
+- npm install
+- npm run dev
+
+이후 나오는 http://주소:5175 에 접속
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fund-dashboard!")
+
+
+if __name__ == "__main__":
+    main()
